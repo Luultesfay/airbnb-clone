37e87537f114d6d491028457ba980cd918468ff2
luul negasi <luultesfay24@gmail.com>
2022-03-02 13:23:07 -0800

    conditional component displayed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Cards from "./components/Cards";
+import Data from "./Data";
+
+console.log(Data);
+
+const displayData = Data.map((data) => (
+  <Cards
+    key={data.id}
+    price={data.price}
+    //images/katie-zaferes.png
+    imgs={`images/${data.imgs}`}
+    reviewCount={data.reivewCount}
+    location={data.location}
+    title={data.Title}
+    rating={data.rating}
+    openSpot={data.openSpot}
+  />
+));
+console.log(displayData);
+
 function App() {
   return (
     <div className="container">
       <NavBar />
       <Hero />
-      <Cards />
+      <section className="card--list">{displayData}</section>
+
+      {/* <Cards
+        imgs={require("./image/katie-zaferes.png")}
+        rating={"5.0"}
+        reviewCount={6}
+        country="Eritrea"
+        Title="Life Lessons with Katie Zaferes"
+        price={136}
+      /> */}
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,30 @@
 import star from "../image/star1.png";
-import pic from "../image/image12.png";
+//import pic from "../image/katie-zaferes.png";
 
-const Cards = () => {
+const Cards = (props) => {
+  console.log(props.openSpot);
+
+  let bageText;
+  if (props.openSpot === 0) {
+    bageText = "SOLD OUT";
+  } else if (props.location === "online") {
+    bageText = "ONLINE";
+  }
+
   return (
     <div className="card">
       <div className="pic">
-        <h5>SOLD OUT</h5>
-        <img src={pic} alt="logo" className="image" />
+        {/* {props.openSpot === 0 && <h5>SOLD OUT</h5>}  conditional statement*/}
+        {bageText && <h5>{bageText}</h5>}
+        <img src={props.imgs} alt="logo" className="image" />
       </div>
       <div className="card-text">
         <img src={star} alt="star" className="star-logo" />
         <h6>
-          5:0(6).USA <br />
-          Life Lesson with katie zafres
+          {`${props.rating}(${props.reviewCount}).${props.location}`} <br />
+          {`${props.title}`}
           <br />
-          <span>FROM $136/Person</span>
+          <span>FROM ${`${props.price}`}/Person</span>
         </h6>
       </div>
     </div>
